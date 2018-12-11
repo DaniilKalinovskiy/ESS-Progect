3ad76e3419271631d5ae2d2f345d41b3f25addc3
DaniilKalinovskiy <42409640+DaniilKalinovskiy@users.noreply.github.com>
2018-12-11 14:23:26 +0200

    profile (MUp)

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
+fName = 'Даниил';
+lName = 'Калиновский';
+age = '20';
+email = 'daniilkalinovskiy@gmail.com';
+sotialNetworks = 'https://vk.com/daniilkalinovskyi';
+hobbies = 'reading';
+city = 'Одесса';
+region = 'Одесская обл.';
+country = 'Украина';
+avatarSRC = 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2012/12/11/20/pg-24-avatar-1-capital.jpg?w968h681';
   constructor() { }
 
   ngOnInit() {
